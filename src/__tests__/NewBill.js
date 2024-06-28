4ab57aa95f65dd5922e6ecc17c3fb60c0b2e772f
Samiya <samii-1989@live.com>
2024-06-28 10:11:34 +0200

    "add integration test POST new bill. "

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -10,6 +10,8 @@ import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
 import router from "../app/Router.js";
 import { expect, jest, test } from "@jest/globals";
+import mockStore from "../__mocks__/store";
+import userEvent from "@testing-library/user-event";
 
 describe("Given I am connected as an employee", () => {
   // SB
@@ -112,13 +114,10 @@ describe("Given I am connected as an employee", () => {
 });
 // });
 
-
-
-
 // SB
 // ajouter un test d'intégration POST new bill.
 describe("Given I am connected as an employee", () => {
-  
+  // beforeEach: Configure l'environnement de test avant chaque test Il espionne l'objet store.bills, configure localStorage pour simuler un utilisateur connecté en tant qu'employé, et initialise l'interface utilisateur.
   beforeEach(() => {
     jest.spyOn(store, "bills");
 
@@ -134,8 +133,6 @@ describe("Given I am connected as an employee", () => {
       })
     );
 
-    
-    
     //
     const root = document.createElement("div");
     root.setAttribute("id", "root");
@@ -143,8 +140,14 @@ describe("Given I am connected as an employee", () => {
     router();
     //
   });
-  
+
+  // afterEach: Restaure les mocks après chaque test pour éviter les interférences entre les tests.
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
   describe("When I add a new bill", () => {
+    // Test de Création d'une Nouvelle Facture
     test("create a new bills POST", async () => {
       const onNavigate = (pathname) => {
         document.body.innerHTML = ROUTES({
@@ -153,14 +156,15 @@ describe("Given I am connected as an employee", () => {
       };
 
       window.onNavigate(ROUTES_PATH.NewBill);
-
+// 1-Configuration du Formulaire et de l'UI:
+      // NewBill est initialisé avec les dépendances nécessaires.
       const newBill = new NewBill({
         document,
         onNavigate,
         store: mockStore,
         localStorage: window.localStorage,
       });
-
+      // Le formulaire est rendu dans le document.body.
       document.body.innerHTML = NewBillUI();
 
       const inputExpenseType = screen.getByTestId("expense-type");
@@ -171,8 +175,75 @@ describe("Given I am connected as an employee", () => {
       const inputPCT = screen.getByTestId("pct");
       const inputCommentary = screen.getByTestId("commentary");
       const inputFile = screen.getByTestId("file");
+// 2-Saisie des Données dans le Formulaire:
+      // Création des Entrées du Formulaire: Le formulaire est initialisé et les champs de texte sont remplis avec les valeurs de test.
+      userEvent.type(inputExpenseType, "Hôtel et logement");
+      userEvent.type(inputExpenseName, "encore");
+      userEvent.type(inputDatePicker, "2004-04-04");
+      userEvent.type(inputAmount, "400");
+      userEvent.type(inputVAT, "80");
+      userEvent.type(inputPCT, "20");
+      userEvent.type(inputCommentary, "séminaire billed");
+      newBill.fileName = "testFile";
+      newBill.fileUrl =
+        "https://test.storage.tld/v0/b/billable-677b6.a…f-1.jpg?alt=media&token=c1640e12-a24b-4b11-ae52-529112e9602a";
+      // const email = "a@a";
 
-      
+
+// 3-Mock de l'API:
+      const handleChangeFile = jest.fn(newBill.handleChangeFile);
+      const handleSubmit = jest.spyOn(newBill, "handleSubmit");
+      const formNewBill = screen.getByTestId("form-new-bill");
+      // mockStore.bills est mocké pour simuler les appels API.
+      mockStore.bills.mockImplementation(() => {
+        return {
+          // La méthode create retourne une promesse résolue avec un objet contenant l'URL du fichier et une clé.
+          create: () => {
+            return Promise.resolve({
+              fileUrl: `${newBill.fileUrl}`,
+              key: "1234",
+            });
+          },
+          update: () => {
+            return Promise.resolve({
+              id: "47qAXb6fIm2zOKkLzMro",
+              vat: "80",
+              fileUrl: `${newBill.fileUrl}`,
+              status: "pending",
+            });
+          },
+        };
+      });
+// 4-Gestion des Événements:
+      // handleChangeFile est mocké et attaché à l'événement change du fichier.
+      inputFile.addEventListener("change", handleChangeFile);
+      // handleSubmit est espionné et attaché à l'événement submit du formulaire.
+      formNewBill.addEventListener("submit", handleSubmit);
+      // Le fichier est téléchargé, et l'événement de soumission du formulaire est déclenché.
+      // userEvent.upload(
+      //   inputFile,
+      //   new File(["(--[IMG]--)"], "testFile.jpg", {
+      //     type: "image/jpg",
+      //   })
+      // );
+      const pngFile = new File(["image"], "is-an-image.png", {
+        type: "image/png",
+      });
+      userEvent.upload(inputFile, pngFile);
+
+      //  Simulation de la soumission du fichier: Le fichier est téléchargé et l'événement de changement de fichier est déclenché. //
+      fireEvent.submit(formNewBill);
+// 5-Assertions:
+      // Vérifie que handleChangeFile et handleSubmit sont appelés une fois.
+      expect(handleChangeFile).toHaveBeenCalled();
+      expect(handleChangeFile).toBeCalledTimes(1);
+      expect(handleSubmit).toHaveBeenCalled();
+      expect(handleSubmit).toBeCalledTimes(1);
+      // Vérifie que mockStore.bills est appelé une fois.
+      expect(mockStore.bills).toHaveBeenCalled();
+      expect(mockStore.bills).toHaveBeenCalledTimes(2);
+      // Vérifie que l'interface utilisateur affiche "Mes notes de frais".
+      expect(screen.getAllByText("Mes notes de frais")).toBeTruthy();
     });
   });
 });
